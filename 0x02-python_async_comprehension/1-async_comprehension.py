f8faf7f18ebafc99f2b8426627c8455b88b03e7f
MarioNageh <marionageh7@gmail.com>
2024-01-16 12:26:21 +0200

    task 1

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    1. Async Comprehensions
+    Write a coroutine called async_comprehension that takes no arguments.
+"""
+
+from typing import List
+
+
+async_generator = __import__("0-async_generator").async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    1. Async Comprehensions
+    """
+    return [i async for i in async_generator()]
